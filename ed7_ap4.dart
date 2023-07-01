a9c133d4987556595c8c23f5460e93810a24e30b
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-01 16:43:09 -0300

    AP4 Concluida

--- ed7_ap4.dart
+++ ed7_ap4.dart
@@ -1,1 +1,46 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+
+  Pessoa eu = Pessoa();
+  eu.nome = "Guilherme";
+  eu.idade = aleatorio.nextInt(99) + 1;
+  eu.altura = (aleatorio.nextDouble() * 1.3) + 1;
+  print(eu);
+}
+
+class Pessoa {
+  String _nome = "";
+  int _idade = 0;
+  double _altura = 0.0;
+
+  String get nome => _nome;
+  int get idade => _idade;
+  double get altura => _altura;
+
+  set nome(String valor) {
+    _nome = valor;
+  }
+
+  set idade(int valor) {
+    if (valor <= 0) {
+      print("A idade deve ser um valor positivo");
+    }
+    _idade = valor;
+  }
+
+  set altura(double valor) {
+    _altura = valor;
+  }
+
+  @override
+  String toString() {
+    String stringPessoa = """ 
+    Nome: $_nome
+    Idade: $_idade
+    Altura: ${_altura.toStringAsFixed(2)}
+    """;
+    return stringPessoa;
+  }
+}
