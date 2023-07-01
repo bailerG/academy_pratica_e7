5b2bc2a7c15863265d89446c1864512c3ace3f41
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-01 16:20:10 -0300

    AP3 Concluida

--- ed7_ap3.dart
+++ ed7_ap3.dart
@@ -1,1 +1,29 @@
-void main() {}
+void main() {
+  final produtos = <Produto>[];
+  final nomes = ['Garrafa', 'Camisa', 'Colar', 'Bermuda', 'Oculos'];
+  final precos = [2.0, 10.0, 8.5, 4.0, 14.0];
+
+  for (var i = 0; i < nomes.length; i++) {
+    Produto produto = Produto(nomes[i], precos[i]);
+    produtos.add(produto);
+
+    final precoDesconto = produto.desconto(5.0);
+    print(
+        "Novo preco do produto ${produtos[i].nome} (com desconto): $precoDesconto");
+  }
+}
+
+class Produto {
+  Produto(this.nome, this.preco);
+  final String nome;
+  double preco;
+
+  double desconto(double percentual) {
+    return preco - (preco * (percentual / 100));
+  }
+
+  @override
+  String toString() {
+    return "$nome $preco";
+  }
+}
