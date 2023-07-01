dee539da81202a82ee30d8458fe1d913b1a1a00e
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-01 15:34:09 -0300

    AP2 Concluida

--- ed7_ap2.dart
+++ ed7_ap2.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  Retangulo ret = Retangulo(7, 18);
+  ret.calcularArea();
+  print(ret);
+}
+
+class Retangulo {
+  Retangulo(this.largura, this.altura);
+  final double largura;
+  final double altura;
+  late final double area;
+
+  void calcularArea() {
+    area = largura * altura;
+  }
+
+  @override
+  String toString() {
+    return "Area do Retangulo: $area";
+  }
+}
